8216bd65fee32b8089d64543356dee7c146e956f
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-10-29 13:06:30 +0100

    app v.0.1.1 creating dbcontext

--- JobSpot/Program.cs
+++ JobSpot/Program.cs
@@ -1,5 +1,13 @@
 
+using JobSpot.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
