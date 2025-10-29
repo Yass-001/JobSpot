b732a13c9e7ebfba3cfb29b1ab23bcc6b2f3f0a9
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-10-29 12:27:44 +0100

    Add project files.

---
+++ JobSpot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobSpot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
