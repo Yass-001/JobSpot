16a379afadd7aa1a259b53a28bc7ce1ae730898a
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-10-31 13:09:25 +0100

    app v.0.1.7 user seeding

--- JobSpot/Data/UserSeeder.cs
+++ JobSpot/Data/UserSeeder.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using JobSpot.Constants;
 
 namespace JobSpot.Data
 {
@@ -8,7 +9,24 @@ namespace JobSpot.Data
         {
             var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
+            // Seed an admin user
 
+            if (await userManager.FindByEmailAsync("admin@jobspot.com") == null)
+            {
+                var adminUser = new IdentityUser
+                {
+                    UserName = "admin@jobspot.com",
+                    Email = "admin@jobspot.com",
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(adminUser, "Admin@123");
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, UserRoles.Admin);
+                }
+            }
         }
     }
 }
