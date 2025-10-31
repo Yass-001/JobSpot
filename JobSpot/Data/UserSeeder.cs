596fcc5644179e1071f25214f12b546eff99f061
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-10-31 12:45:07 +0100

    app v.0.1.6 roles seeded

---
+++ JobSpot/Data/UserSeeder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JobSpot.Data
+{
+    public class UserSeeder
+    {
+        public static async Task SeedUsersAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+
+        }
+    }
+}
