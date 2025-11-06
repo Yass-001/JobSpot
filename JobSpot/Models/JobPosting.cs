aaebf88c1ee24cf2f606a4248df7a501af59db61
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-11-06 12:54:13 +0100

    app v.0.2.2 creating JobPosting model

---
+++ JobSpot/Models/JobPosting.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobSpot.Models
+{
+    public class JobPosting
+    {
+        [Key] // Primary key attribute - maybe not needed if convention is followed
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string Location { get; set; }
+        
+        public DateTime PostedDate { get; set; } // - ?! Default to current date when creating a new posting
+
+
+        public JobPosting() { }
+
+
+
+    }
+}
