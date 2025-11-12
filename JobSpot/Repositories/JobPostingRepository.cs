5659dd92b043a28f1392a305f38af482cb4ed7b8
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-11-12 11:03:16 +0100

    app v.0.2.5 JobPostingRepository done

--- JobSpot/Repositories/JobPostingRepository.cs
+++ JobSpot/Repositories/JobPostingRepository.cs
@@ -1,5 +1,6 @@
 ﻿using JobSpot.Data;
 using JobSpot.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobSpot.Repositories
 {
@@ -10,29 +11,61 @@ namespace JobSpot.Repositories
         {
             _context = appDbContext;
         }
-        public Task AddAsync(JobPosting entity)
+        public async Task AddAsync(JobPosting entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity), "JobPosting entity cannot be null");
+            }
+            await _context.JobPostings.AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(Guid id)
+        public async Task DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var jobPosting = await _context.JobPostings.FindAsync(id);
+
+            if (jobPosting != null)
+            {
+                _context.JobPostings.Remove(jobPosting);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException($"JobPosting with Id {id} not found.");
+            }
         }
 
-        public Task<IEnumerable<JobPosting>> GetAllAsync()
+        public async Task<IEnumerable<JobPosting>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _context.JobPostings.ToListAsync();
         }
 
-        public Task<JobPosting> GetByIdAsync(Guid id)
+        public async Task<JobPosting> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var jobPosting = await _context.JobPostings.FindAsync(id);
+            if (jobPosting != null)
+            {
+                return jobPosting;
+            }
+            throw new KeyNotFoundException($"JobPosting with Id {id} not found.");
         }
 
-        public Task UpdateAsync(JobPosting entity)
+        public async Task UpdateAsync(JobPosting entity)
         {
-            throw new NotImplementedException();
+            _context.JobPostings.Update(entity);
+            //await _context.JobPostings
+            //    .Where(jp => jp.Id == entity.Id)
+            //    .ExecuteUpdateAsync(jp => jp
+            //        .SetProperty(jp => jp.Title, entity.Title)
+            //        .SetProperty(jp => jp.Description, entity.Description)
+            //        .SetProperty(jp => jp.Company, entity.Company)
+            //        .SetProperty(jp => jp.Location, entity.Location)
+            //        .SetProperty(jp => jp.IsApproved, entity.IsApproved));
+            await _context.SaveChangesAsync();
+
+            //_context.Entry(jobPosting).CurrentValues.SetValues(entity);
+            //await _context.SaveChangesAsync();
         }
     }
 }
