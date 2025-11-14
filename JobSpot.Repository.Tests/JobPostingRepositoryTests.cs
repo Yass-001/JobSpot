72dad5a596f8e9e30ae71ca114745f5561e02170
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-11-14 13:13:34 +0100

    app v.0.2.6 Unit Tests started to write

---
+++ JobSpot.Repository.Tests/JobPostingRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using JobSpot.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSpot.Repository.Tests
+{
+    internal class JobPostingRepositoryTests
+    {
+        private readonly DbContextOptions<AppDbContext> _dbContextOptions;
+
+        public JobPostingRepositoryTests()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: $"InMemoryDb-{Guid.NewGuid()}")
+                .Options;
+        }
+
+        private AppDbContext CreateDbContext() => new AppDbContext(_dbContextOptions);
+    }
+}
+ 
