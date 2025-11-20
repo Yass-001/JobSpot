847437610d08f1adc09934fbd29cffce126fd60b
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-11-20 11:11:36 +0100

    app v.0.2.7 creating new controller

---
+++ JobSpot/Controllers/JobPostingsController.cs
@@ -0,0 +1,24 @@
+﻿using JobSpot.Models;
+using JobSpot.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSpot.Controllers
+{
+    public class JobPostingsController : Controller
+    {
+        private readonly IRepository<JobPosting> _jobPostingRepository;
+        private readonly UserManager<IdentityUser> _userManager; // IUserManager injection - ?!
+        
+        public JobPostingsController(IRepository<JobPosting> repository, UserManager<IdentityUser> userManager)
+        {
+            _jobPostingRepository = repository;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- JobSpot/Program.cs
+++ JobSpot/Program.cs
@@ -55,7 +55,7 @@ app.MapStaticAssets();
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=JobPostings}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
