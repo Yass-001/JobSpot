00f760cf3f787b776d9c741e081e77d75e47ec65
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-12-03 13:12:21 +0100

    app v.0.2.9 adding Delete controller

--- JobSpot/Controllers/JobPostingsController.cs
+++ JobSpot/Controllers/JobPostingsController.cs
@@ -58,29 +58,37 @@ namespace JobSpot.Controllers
             return View(jobPostingVM);
         }
 
+        [HttpDelete]
         [Authorize(Roles = "Admin,Employer")]
         public async Task<IActionResult> Delete(Guid id)
         {
-            var jobPosting = await _jobPostingRepository.GetByIdAsync(id);
-            if (jobPosting == null)
-            {
-                return NotFound();
-            }
-            return View(jobPosting);
+            return Ok();
         }
 
-        [HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken] // Uncomment if CSRF protection is needed
-        [Authorize(Roles = "Admin,Employer")]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var jobPosting = await _jobPostingRepository.GetByIdAsync(id);
-            if (jobPosting == null)
-            {
-                return NotFound();
-            }
-            await _jobPostingRepository.DeleteAsync(jobPosting.Id);
-            return RedirectToAction(nameof(Index));
-        }
+
+        //[Authorize(Roles = "Admin,Employer")]
+        //public async Task<IActionResult> Delete(Guid id)
+        //{
+        //    var jobPosting = await _jobPostingRepository.GetByIdAsync(id);
+        //    if (jobPosting == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(jobPosting);
+        //}
+
+        //[HttpPost, ActionName("Delete")]
+        ////[ValidateAntiForgeryToken] // Uncomment if CSRF protection is needed
+        //[Authorize(Roles = "Admin,Employer")]
+        //public async Task<IActionResult> DeleteConfirmed(Guid id)
+        //{
+        //    var jobPosting = await _jobPostingRepository.GetByIdAsync(id);
+        //    if (jobPosting == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    await _jobPostingRepository.DeleteAsync(jobPosting.Id);
+        //    return RedirectToAction(nameof(Index));
+        //}
     }
 }
