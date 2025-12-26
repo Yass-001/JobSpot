65f2d6d15eea691fb56224af96b9734a155bf436
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-12-26 15:41:06 +0100

    app v.0.3.3 Edit controller added, changed Index page style

--- JobSpot/Interfaces/IUserManager.cs
+++ JobSpot/Interfaces/IUserManager.cs
@@ -9,5 +9,14 @@ namespace JobSpot.Interfaces
         Task<IdentityUser> GetUserAsync(ClaimsPrincipal user);
 
         // Add other necessary user management methods as needed
+
+        // Checks if a user is in a specific role
+        //Task<bool> IsInRoleAsync(IdentityUser user, string role);
+
+        // Finds a user by their ID
+        //Task<IdentityUser> FindByIdAsync(string userId);
+
+        // Finds a user by their username/email
+        //Task<IdentityUser> FindByNameAsync(string userName);
     }
 }
