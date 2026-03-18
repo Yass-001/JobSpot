4b4081908383fe5c29d29266fdc8631faa7e155e
Yuriy Kutsenko <yasssin3008@gmail.com>
2026-03-18 14:57:35 +0100

    App v.0.4.2 Authentication in work(Google)

--- JobSpot/Areas/Identity/Pages/Account/Google.cshtml.cs
+++ JobSpot/Areas/Identity/Pages/Account/Google.cshtml.cs
@@ -1,14 +1,29 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JobSpot.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class GoogleModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGet(string returnUrl)
         {
+            //await HttpContext.ChallengeAsync("Google");
+
+            if (!Url.IsLocalUrl(returnUrl))
+            {
+                throw new InvalidOperationException("Invalid return URL."); // don`t do for production, just for testing?!
+            }
+
+            var properties = new AuthenticationProperties 
+            { 
+                RedirectUri = returnUrl 
+            };
+
+            return Challenge(properties, "Google");
         }
     }
 }
