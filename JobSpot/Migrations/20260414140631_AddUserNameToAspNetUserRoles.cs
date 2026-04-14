e74bb0bdb058156f0134cfaf7c9d17935d6dc459
Yuriy Kutsenko <yasssin3008@gmail.com>
2026-04-14 16:58:33 +0200

    App v.0.4.2 error adding 'name' property to AspNetUserRoles

---
+++ JobSpot/Migrations/20260414140631_AddUserNameToAspNetUserRoles.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobSpot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNameToAspNetUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "AspNetUserRoles",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "AspNetUserRoles");
+        }
+    }
+}
