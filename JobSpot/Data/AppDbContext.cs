3c853d4b48c17432c042d419dc7e3f8d9dab8c09
Yuriy Kutsenko <yasssin3008@gmail.com>
2026-04-14 16:58:33 +0200

    App v.0.4.2 error adding 'name' property to AspNetUserRoles

--- JobSpot/Data/AppDbContext.cs
+++ JobSpot/Data/AppDbContext.cs
@@ -1,16 +1,30 @@
 ﻿using JobSpot.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobSpot.Data
 {
-    public class AppDbContext : IdentityDbContext 
+    // You cannot customize AspNetUserRoles without introducing your own user class.
+    public class AppDbContext : IdentityDbContext<IdentityUser, IdentityRole, string,
+    IdentityUserClaim<string>, ApplicationUserRole,
+    IdentityUserLogin<string>, IdentityRoleClaim<string>,
+    IdentityUserToken<string>>
     {
         public DbSet<JobPosting> JobPostings { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUserRole>()
+                .Property(ur => ur.UserName)
+                .HasMaxLength(256);
         }
     }
 }
---
+++ JobSpot/Data/ApplicationUserRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JobSpot.Data
+{
+    public class ApplicationUserRole : IdentityUserRole<string>
+    {
+        public string UserName { get; set; }
+    }
+}
