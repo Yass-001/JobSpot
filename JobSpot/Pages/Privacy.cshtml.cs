14cd5f361794a418924d725f037f4f785ad4c8d1
Yuriy Kutsenko <yasssin3008@gmail.com>
2026-04-14 17:28:00 +0200

    app v.0.4.3 Update the body style in site.css to add a background color

---
+++ JobSpot/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobSpot.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+// F:\CS_My_Projects\JobSpot\Pages\Privacy.cshtml.cs
